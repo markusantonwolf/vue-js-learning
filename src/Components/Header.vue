e157595ab1346426320e05ae26d93da4969aa02f
Markus Wolf <markus.wolf@craftplaces.com>
2018-10-25 21:55:21 +0200

    First changes...

---
+++ src/Components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1>
+      {{ title }}
+    </h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      title: 'Welcome to an other Vue.ja App'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import "../assets/styles/variables.scss";
+
+  HEADER {
+    background-color: $background;
+  }
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Header from './Components/Header.vue'
+
+import Ninjas from './Ninjas.vue'
+
+Vue.component('Header', Header);
+
+Vue.component('Ninjas', Ninjas);
 
 new Vue({
   el: '#app',
